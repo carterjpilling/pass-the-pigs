2268da67ea0a2e5cb25f8d9da5e6cc19841dbac3
Cj <carterjpilling@gmail.com>
2021-01-13 16:47:52 -0700

    Added some sweet colors

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Paper, Button, Typography } from '@material-ui/core'
 import './Game.css'
 // import { BiDice1, BiDice2, BiDice3, BiDice4, BiDice5, BiDice6 } from 'react-icons/bi'
+import { FaDiceOne, FaDiceTwo, FaDiceThree, FaDiceFour, FaDiceFive, FaDiceSix, FaDice } from 'react-icons/fa'
 import { GiPig } from 'react-icons/gi'
 
 
@@ -28,6 +29,9 @@ export default function Game() {
   const [die1, setDie1] = useState(0)
   const [die2, setDie2] = useState(0)
 
+  // const [dice1, setDice1] = useState(FaDice)
+  // const [dice2, setDice2] = useState(FaDice)
+
   const [oneHit, setOneHit] = useState(false)
 
   const [winnerState, setWinnerState] = useState(false)
@@ -49,6 +53,45 @@ export default function Game() {
     }
   }, [player1Score, player2Score])
 
+  // useEffect(() => {
+  //   console.log(dice1, 'beginning')
+  //   const FaDice = 0
+  //   const FaDiceOne = 1
+  //   const FaDiceTwo = 2
+  //   const FaDiceThree = 3
+  //   const FaDiceFour = 4
+  //   const FaDiceFive = 5
+  //   const FaDiceSix = 6
+
+  //   switch (die1) {
+  //     case FaDice:
+  //       setDice1(FaDice)
+  //       break
+  //     case FaDiceOne:
+  //       setDice1(FaDiceOne)
+  //       break
+  //     case FaDiceTwo:
+  //       setDice1(FaDiceTwo)
+  //       break
+  //     case FaDiceThree:
+  //       setDice1(FaDiceThree)
+  //       break
+  //     case FaDiceFour:
+  //       setDice1(FaDiceFour)
+  //       break
+  //     case FaDiceFive:
+  //       setDice1(FaDiceFive)
+  //       break
+  //     case FaDiceSix:
+  //       setDice1(FaDiceSix)
+  //       break
+  //     default:
+  //       return null
+  //   }
+
+  //   console.log(dice1, 'end')
+
+  // }, [die1])
 
   useEffect(() => {
     if (die1 === 1 || die2 === 1) {
@@ -84,6 +127,9 @@ export default function Game() {
       setCurrentScore2(0)
       setCurrentPlayer(1)
     }
+
+    setDie1(0)
+    setDie2(0)
 
   }
 
@@ -124,13 +170,12 @@ export default function Game() {
     <div>
       <Paper className="paper">
         <div className="player1-side">
-          <div>
+          <div className='player-pig-container'>
+            <h3>Player 1</h3>
             {currentPlayer === 1 &&
-              <GiPig />
+              <GiPig color='rgb(69,19,28)' />
             }
-
           </div>
-          <h3>Player 1</h3>
           <h2>{player1Score}</h2>
           <div className="current">
             <p>CURRENT</p>
@@ -141,12 +186,22 @@ export default function Game() {
           <Button type='submit' variant='contained' onClick={() => newGame()}>New Game</Button>
           <div className="dice-container">
             <h1>
-              {die1}
-              {/* <BiDice1 /> */}
+              {die1 === 0 && <FaDice color='rgb(69,19,28)' />}
+              {die1 === 1 && <FaDiceOne color='rgb(69,19,28)' />}
+              {die1 === 2 && <FaDiceTwo color='rgb(69,19,28)' />}
+              {die1 === 3 && <FaDiceThree color='rgb(69,19,28)' />}
+              {die1 === 4 && <FaDiceFour color='rgb(69,19,28)' />}
+              {die1 === 5 && <FaDiceFive color='rgb(69,19,28)' />}
+              {die1 === 6 && <FaDiceSix color='rgb(69,19,28)' />}
             </h1>
             <h1>
-              {die2}
-              {/* <BiDice2 /> */}
+              {die2 === 0 && <FaDice color='rgb(69,19,28)' />}
+              {die2 === 1 && <FaDiceOne color='rgb(69,19,28)' />}
+              {die2 === 2 && <FaDiceTwo color='rgb(69,19,28)' />}
+              {die2 === 3 && <FaDiceThree color='rgb(69,19,28)' />}
+              {die2 === 4 && <FaDiceFour color='rgb(69,19,28)' />}
+              {die2 === 5 && <FaDiceFive color='rgb(69,19,28)' />}
+              {die2 === 6 && <FaDiceSix color='rgb(69,19,28)' />}
             </h1>
           </div>
           {winnerState === true ? <Typography className="game-button-container">Winner is Player {winnerPlayer}!</Typography> :
@@ -164,13 +219,13 @@ export default function Game() {
           }
         </div>
         <div className="player2-side">
-          <div>
+          <div className='player-pig-container'>
+            <h3>Player 2</h3>
             {currentPlayer === 2 &&
-              <GiPig />
+              <GiPig color='rgb(69,19,28)' />
             }
 
           </div>
-          <h3>Player 2</h3>
           <h2>{player2Score}</h2>
           <div className="current">
             <p>CURRENT</p>
