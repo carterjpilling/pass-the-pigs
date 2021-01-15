a69b9f926b3ef5729de20ff90697d17ddfef7a23
Cj <carterjpilling@gmail.com>
2021-01-15 11:21:12 -0700

    Changed Ga

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { useEffect } from 'react'
 import Game from './Components/Game'
 import ReactGA from 'react-ga';
 
 
-ReactGA.initialize('G-TK3CEES8CS')
-
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize('UA-185129485-1')
+    ReactGA.pageview('/');
+
+  }, [])
   return (
     <div className="App">
       <Game />
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-185129485-1')
-ReactGA.pageview(window.location.pathname + window.location.search);
+
+
 
 ReactDOM.render(
   <React.StrictMode>
