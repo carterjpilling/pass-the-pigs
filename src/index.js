966339fd03151e16624fd073704e93aaea384b4e
Cj <carterjpilling@gmail.com>
2021-01-15 11:10:25 -0700

    Changed value

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReactGA from 'react-ga';
-ReactGA.initialize('G-TK3CEES8CS')
+ReactGA.initialize('UA-185129485-1')
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
